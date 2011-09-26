f4bf5a36e72d3c5c5940d1c9f9a1695780e353f8
bsnizek <bs@metascapes.org>
2011-09-26 14:02:44 +0200

    new type in orm

---
+++ JMapMatchingGUI/src/org/life/sl/orm/Bicycletype.java
@@ -0,0 +1,27 @@
+package org.life.sl.orm;
+
+public class Bicycletype {
+	
+	private int id;
+	private String descr;
+
+
+	public String getDescr() {
+		return descr;
+	}
+
+	public void setDescr(String descr) {
+		this.descr = descr;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+}
+    
