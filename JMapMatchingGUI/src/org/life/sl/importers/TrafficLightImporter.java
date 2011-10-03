d359c89e0010b40a3c65308b663fd0a77d2c92dd
bsnizek <bs@metascapes.org>
2011-10-03 17:22:02 +0200

    + stax and a very fast importer

--- JMapMatchingGUI/src/org/life/sl/importers/TrafficLightImporter.java
+++ JMapMatchingGUI/src/org/life/sl/importers/TrafficLightImporter.java
@@ -1,6 +1,7 @@
 package org.life.sl.importers;
 
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 
@@ -21,12 +22,15 @@ import org.opengis.referencing.operation.TransformException;
 import org.openstreetmap.josm.io.IllegalDataException;
 
 
-import java.io.File;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+
+import javax.xml.stream.FactoryConfigurationError;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -39,102 +43,129 @@ public class TrafficLightImporter {
 	/**
 	 * @param args
 	 */
-	
-	public TrafficLightImporter() {
-		
-//		http://download.bbbike.org/osm/bbbike/Copenhagen/
-// 		2011-Sep-29 14:13:52
-		
-		
-	}
-	
-	public void read(String filename) throws IllegalDataException, IOException, FactoryException, ParserConfigurationException, SAXException, TransformException {
-		readOSMFilefromStream(filename);
-		
-		
-	}
-	
-	private void readOSMFilefromStream(String filename) throws ParserConfigurationException, IOException, FactoryException, SAXException, TransformException {
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 
-		DocumentBuilder db = dbf.newDocumentBuilder();
+	public TrafficLightImporter() {
+
+		//		http://download.bbbike.org/osm/bbbike/Copenhagen/
+		// 		2011-Sep-29 14:13:52
+
+
+	}
+
+	public void read(String filename) throws IllegalDataException, IOException, FactoryException, ParserConfigurationException, SAXException, TransformException, XMLStreamException, FactoryConfigurationError {
+		//readOSMFilefromStream(filename);
+		readOSMFileWithStax(filename);
+
+	}
+
+	private void readOSMFileWithStax(String filename) throws XMLStreamException, FactoryConfigurationError, IOException, FactoryException, TransformException {
+
 
 		com.vividsolutions.jts.geom.GeometryFactory fact = new com.vividsolutions.jts.geom.GeometryFactory();
-		
+
 		ProjectionUtil pu = new ProjectionUtil();
-		
-		Document dom = db.parse(filename);
-		
+
 		CoordinateReferenceSystem crs_from = pu.getCRS("prj/osm.prj");
 		CoordinateReferenceSystem crs_to = pu.getCRS("prj/gps.prj");
-		
+
 		CoordinateTransformer ct = new CoordinateTransformer(crs_from, crs_to);
 
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
+
+		FileInputStream fileInputStream = 
+				new FileInputStream(filename);
+		XMLStreamReader staxXmlReader =
+				XMLInputFactory.newInstance().
+				createXMLStreamReader(fileInputStream);
+
+		String lat_string = null;
+		String lon_string = null;
+		String id_string = null;
+		boolean hasSignal = false;
+
+		int counter = 0;
 		
-		org.w3c.dom.Element docEle = dom.getDocumentElement();
-		NodeList nl = docEle.getElementsByTagName("node");
-		System.out.println(nl.getLength());
-		
-		if(nl != null && nl.getLength() > 0) {
-			for(int i = 0 ; i < nl.getLength();i++) {
-		
-				Element el = (Element) nl.item(i);
-				NodeList tags = el.getElementsByTagName("tag");
-				
-				boolean hasTrafficLight = false;
-				
-				if(tags != null && tags.getLength() > 0) {
-					
-					for(int j = 0 ; j < nl.getLength();j++) {
-						
-						Element tag_el = (Element) tags.item(j);
-						if (tag_el != null) {
-						String v = tag_el.getAttribute("v");
-							hasTrafficLight = true;
-						}					
+		for (int event = staxXmlReader.next(); event != XMLStreamConstants.END_DOCUMENT; event = staxXmlReader.next()) {
+			switch (event) {
+			case XMLStreamConstants.START_DOCUMENT:
+				// System.out.println("Start document " + staxXmlReader.getLocalName());
+				break;
+			case XMLStreamConstants.START_ELEMENT:
+				// System.out.println("Start element " + staxXmlReader.getLocalName());
+				if (staxXmlReader.getLocalName() == "node") {
+					lat_string = staxXmlReader.getAttributeValue("", "lat");
+					lon_string = staxXmlReader.getAttributeValue("", "lon");
+					id_string = staxXmlReader.getAttributeValue("", "id");
+				}
+
+				if (staxXmlReader.getLocalName() == "tag") { 
+					String v = staxXmlReader.getAttributeValue("", "v");
+
+					if (v.equals("traffic_signals")) {
+						hasSignal = true;
 					}
 				}
-				
-				if (hasTrafficLight) {
-					String lat_s = el.getAttribute("lat");
-					String lon_s = el.getAttribute("lon");
-					String id_s = el.getAttribute("id");
+
+
+				break;
+			case XMLStreamConstants.END_ELEMENT:
+				// System.out.println("End element " + staxXmlReader.getLocalName());
+				if (staxXmlReader.getLocalName() == "node") {
+					if (hasSignal) {
+						System.out.println ("WRITING node " + id_string + " (" + lat_string + "/" + lon_string + ")");
+						int id = Integer.valueOf(id_string);
+
+						Float lat = Float.valueOf(lat_string);
+						Float lon = Float.valueOf(lon_string);
+
+
+						Coordinate c = new Coordinate(lon, lat); // z = 0, no elevation
+						Coordinate coord_transformed = ct.transform(c);
+
+						Point point = fact.createPoint(coord_transformed);
+
+						Trafficlight l = new Trafficlight();
+
+						l.setGeometry(point);
+						l.setId(id);
+
+						System.out.print(".");
+						session.save(l);
+						
+						counter++;
+						if (counter == 20) {
+							session.getTransaction().commit();
+							session = HibernateUtil.getSessionFactory().getCurrentSession();
+							session.beginTransaction();
+							counter = 0;
+							System.out.println("flushed");
+						}
+						
 					
-					int id = Integer.valueOf(id_s);
-					
-					Float lat = Float.valueOf(lat_s);
-					Float lon = Float.valueOf(lon_s);
-					
-					
-					Coordinate c = new Coordinate(lon, lat); // z = 0, no elevation
-					Coordinate coord_transformed = ct.transform(c);
-					
-					Point point = fact.createPoint(coord_transformed);
-					
-					Trafficlight l = new Trafficlight();
-					
-					l.setGeometry(point);
-					l.setId(id);
-					
-					System.out.print(".-");
-					session.save(l);
+					}
+					hasSignal = false;
 				}
-				
+				break;
+			default:
+				break;
 			}
+	
+			
 		}
 		
-		session.getTransaction().commit();
+		System.out.println("import finished");
+
+
 
 	}
 
-	public static void main(String[] args) throws IllegalDataException, IOException, FactoryException, ParserConfigurationException, SAXException, TransformException {
-		
+	public static void main(String[] args) throws IllegalDataException, IOException, FactoryException, ParserConfigurationException, SAXException, TransformException, XMLStreamException, FactoryConfigurationError {
+
 		TrafficLightImporter tli = new TrafficLightImporter();
 		tli.read("/Users/besn/Downloads/Copenhagen2.osm");
 		// tli.read("/Users/besn/Downloads/trafficlights.osm");
-		
+
 	}
 
 }
