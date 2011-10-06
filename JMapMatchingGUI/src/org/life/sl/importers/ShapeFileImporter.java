493d0537c2e82aebaa35af1ad76d285bee785c4f
bsnizek <bs@metascapes.org>
2011-10-06 18:17:46 +0200

    HSP 's wishes importer adapted

--- JMapMatchingGUI/src/org/life/sl/importers/ShapeFileImporter.java
+++ JMapMatchingGUI/src/org/life/sl/importers/ShapeFileImporter.java
@@ -29,113 +29,134 @@ public class ShapeFileImporter {
 	public void dumpToPostgresql() {
 		ArrayList<Node> nodes = pSg.getNodes();
 		Iterator<Node> iter = nodes.iterator();
-		
+
 		System.out.println("Writing nodes...");
 		int i = 0;
 		HashMap<Node, Integer> node__nodeId = new HashMap<Node, Integer>();
 		while (iter.hasNext()) {
 			Node n = iter.next();
-			
+
 			OSMNode osmNode = new OSMNode();
 			osmNode.setGeometry(fact.createPoint(n.getCoordinate()));
 			// @SuppressWarnings("unchecked")
 			// HashMap<String, Integer> data = (HashMap<String, Integer>) n.getData();
 			// int id = data.get("id");
 			osmNode.setId(i); //  todo set this properly
-			
+
 			node__nodeId.put(n, i);
 			session.save(osmNode);
 			i++;
 		}
-		
+
 		Collection<Edge> edges = pSg.getEdges();
 		Iterator<Edge> iter2 = edges.iterator();
-		
+
 		System.out.println("Writing edges...");
 		int j=0;
-		
+
 		while (iter2.hasNext()){
 			Edge e = iter2.next();
-			
+
 			OSMEdge osmEdge = new OSMEdge();
 			@SuppressWarnings("unchecked")
 			HashMap<String, Object> data2 = (HashMap<String, Object>) e.getData();
 			Object geom = data2.get("geometry");
+
+			Object bctyp_s = data2.get("BICYCLETYP");
+			if (bctyp_s != null) {
+				Integer bint = new Integer((Integer) bctyp_s);
+				Short bicycle = bint.shortValue();
+				osmEdge.setBicycletype(bicycle);
+			}
+
+
+			Object cycleway_s = data2.get("CYCLEWAYTY");
+			if (cycleway_s != null) {
+				Integer cwint = new Integer((Integer) cycleway_s);
+				Short cycleway = cwint.shortValue();
+				osmEdge.setCyclewaytype(cycleway);
+			}
+
+			Object foottype_s = data2.get("FOOTTYPE");
+			if (foottype_s != null) {
+				Integer fint = new Integer((Integer) foottype_s);
+				Short foot = fint.shortValue();
+				osmEdge.setFoottype(foot);
+			}
+
+
+			Object hwt_s = data2.get("HIGHWAYTYP");
+			if (hwt_s != null) {
+				Integer hwt = new Integer((Integer) hwt_s);
+				Short highway = hwt.shortValue();
+				osmEdge.setHighwaytype(highway);
+
+			}
+
+			Object sgrd_s = data2.get("SEGREGATED");
+			if (sgrd_s != null) {
+				Integer sgrd = new Integer((Integer) sgrd_s);
+				Short segregated = sgrd.shortValue();
+				osmEdge.setSegregatedtype(segregated);
+			}
 			
-			Integer bint = new Integer((Integer) data2.get("BICYCLETYP"));
-			Short bicycle = bint.shortValue();
-			
-			Integer cwint = new Integer((Integer) data2.get("CYCLEWAYTY"));
-			Short cycleway = cwint.shortValue();
-			
-			Integer fint = new Integer((Integer) data2.get("FOOTTYPE"));
-			Short foot = fint.shortValue();
-			
-			Integer hwt = new Integer((Integer) data2.get("HIGHWAYTYP"));
-			Short highway = hwt.shortValue();
-			
-			Integer sgrd = new Integer((Integer) data2.get("SEGREGATED"));
-			Short segregated = sgrd.shortValue();
-			
+			Object roadname_o = data2.get("ROADNAME");
+			if (roadname_o != null) {
+				String roadname = (String) roadname_o;
+				osmEdge.setRoadname(roadname);
+			}
+
 			// The HSP-specific stuff
-			
-			Integer envtp = new Integer((Integer) data2.get("ENVTYPE"));
+
+			Integer envtp = new Integer((Integer) data2.get("EnvType"));
 			Short envType = envtp.shortValue();
 
-			Integer cyktp = new Integer((Integer) data2.get("CYKTYPE"));
+			Integer cyktp = new Integer((Integer) data2.get("CykType"));
 			Short cykType = cyktp.shortValue();
-			
-			float groenPct = new Float((Float) data2.get("GROENPCT"));
-			float groenM = new Float((Float) data2.get("GROENM"));
-			
-			String roadname = (String) data2.get("ROADNAME");
-			
+
+			Double groenPct = new Double((Double) data2.get("GroenPct"));
+			Double groenM = new Double((Double) data2.get("GroenM"));
+
+
 			LineString ls = (LineString) geom;
-			
+
 			osmEdge.setGeometry(ls);
 			osmEdge.setId(j);
-			osmEdge.setBicycletype(bicycle);
-			osmEdge.setCyclewaytype(cycleway);
-			osmEdge.setFoottype(foot);
-			osmEdge.setHighwaytype(highway);
-			osmEdge.setRoadname(roadname);
-			osmEdge.setSegregatedtype(segregated);
-			
-			osmEdge.setEnvType(envType);
-			osmEdge.setCykType(cykType);
-			osmEdge.setGroenPct(groenPct);
-			osmEdge.setGroenM(groenM);
-			
+			osmEdge.setEnvtype(envType);
+			osmEdge.setCyktype(cykType);
+			osmEdge.setGroenpct(groenPct);
+			osmEdge.setGroenm(groenM);
+
 			Node from_node = e.getDirEdge(0).getFromNode();
 			Node to_node = e.getDirEdge(0).getToNode();
-			
+
 			Integer from_node_id = node__nodeId.get(from_node);
 			Integer to_node_id = node__nodeId.get(to_node);
-			
+
 			osmEdge.setFromnode(from_node_id);
 			osmEdge.setTonode(to_node_id);
 			osmEdge.setLength(ls.getLength());
-			
+
 			session.save(osmEdge);
 			j++;
 			System.out.print(".");
 		}
 		session.getTransaction().commit();
 		System.out.println("Import finished");
-		
-		
+
+
 	}
-	
+
 	public ShapeFileImporter(String shapefile) throws IOException {
 		session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 		pSg = new PathSegmentGraph(shapefile);
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		ShapeFileImporter oSMPI = new ShapeFileImporter("testdata/CPH_OSM/CPH_OSM_Bikeability.shp");
 		oSMPI.dumpToPostgresql();
 		System.out.println("Finished");
 	}
-	
+
 }
--- JMapMatchingGUI/src/org/life/sl/orm/OSMEdge.java
+++ JMapMatchingGUI/src/org/life/sl/orm/OSMEdge.java
@@ -3,10 +3,100 @@ package org.life.sl.orm;
 import com.vividsolutions.jts.geom.LineString;
 
 public class OSMEdge {
-	
+
 	private int id;
 	private LineString geometry;
 	private int fromnode;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public LineString getGeometry() {
+		return geometry;
+	}
+	public void setGeometry(LineString geometry) {
+		this.geometry = geometry;
+	}
+	public int getFromnode() {
+		return fromnode;
+	}
+	public void setFromnode(int fromnode) {
+		this.fromnode = fromnode;
+	}
+	public int getTonode() {
+		return tonode;
+	}
+	public void setTonode(int tonode) {
+		this.tonode = tonode;
+	}
+	public double getLength() {
+		return length;
+	}
+	public void setLength(double length) {
+		this.length = length;
+	}
+	public String getRoadname() {
+		return roadname;
+	}
+	public void setRoadname(String roadname) {
+		this.roadname = roadname;
+	}
+	public Short getHighwaytype() {
+		return highwaytype;
+	}
+	public void setHighwaytype(Short highwaytype) {
+		this.highwaytype = highwaytype;
+	}
+	public Short getCyclewaytype() {
+		return cyclewaytype;
+	}
+	public void setCyclewaytype(Short cyclewaytype) {
+		this.cyclewaytype = cyclewaytype;
+	}
+	public Short getFoottype() {
+		return foottype;
+	}
+	public void setFoottype(Short foottype) {
+		this.foottype = foottype;
+	}
+	public Short getBicycletype() {
+		return bicycletype;
+	}
+	public void setBicycletype(Short bicycletype) {
+		this.bicycletype = bicycletype;
+	}
+	public Short getSegregatedtype() {
+		return segregatedtype;
+	}
+	public void setSegregatedtype(Short segregatedtype) {
+		this.segregatedtype = segregatedtype;
+	}
+	public Short getEnvtype() {
+		return envtype;
+	}
+	public void setEnvtype(Short envtype) {
+		this.envtype = envtype;
+	}
+	public Short getCyktype() {
+		return cyktype;
+	}
+	public void setCyktype(Short cyktype) {
+		this.cyktype = cyktype;
+	}
+	public double getGroenpct() {
+		return groenpct;
+	}
+	public void setGroenpct(double groenpct) {
+		this.groenpct = groenpct;
+	}
+	public double getGroenm() {
+		return groenm;
+	}
+	public void setGroenm(double groenm) {
+		this.groenm = groenm;
+	}
 	private int tonode;
 	private double length;
 	private String roadname;
@@ -15,137 +105,13 @@ public class OSMEdge {
 	private Short foottype;
 	private Short bicycletype;
 	private Short segregatedtype;
-	
+
 	// HSP specific attributes
-	
-	private Short EnvType;
-	private Short CykType;
-	private double GroenPct;
-	private double GroenM;
-	
-	
-	
-	public Short getEnvType() {
-		return EnvType;
-	}
 
-	public void setEnvType(Short envType) {
-		EnvType = envType;
-	}
-
-	public Short getCykType() {
-		return CykType;
-	}
-
-	public void setCykType(Short cykType) {
-		CykType = cykType;
-	}
-
-	public double getGroenPct() {
-		return GroenPct;
-	}
-
-	public void setGroenPct(double groenPct) {
-		GroenPct = groenPct;
-	}
-
-	public double getGroenM() {
-		return GroenM;
-	}
-
-	public void setGroenM(double groenM) {
-		GroenM = groenM;
-	}
+	private Short envtype;
+	private Short cyktype;
+	private double groenpct;
+	private double groenm;
 
 
-	
-	
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public int getFromnode() {
-		return fromnode;
-	}
-
-	public void setFromnode(int fromnode) {
-		this.fromnode = fromnode;
-	}
-
-	public int getTonode() {
-		return tonode;
-	}
-
-	public void setTonode(int tonode) {
-		this.tonode = tonode;
-	}
-
-	public double getLength() {
-		return length;
-	}
-
-	public void setLength(double length) {
-		this.length = length;
-	}
-
-	public String getRoadname() {
-		return roadname;
-	}
-
-	public void setRoadname(String roadname) {
-		this.roadname = roadname;
-	}
-
-	public Short getHighwaytype() {
-		return highwaytype;
-	}
-
-	public void setHighwaytype(Short highwaytype) {
-		this.highwaytype = highwaytype;
-	}
-
-	public Short getCyclewaytype() {
-		return cyclewaytype;
-	}
-
-	public void setCyclewaytype(Short cyclewaytype) {
-		this.cyclewaytype = cyclewaytype;
-	}
-
-	public Short getFoottype() {
-		return foottype;
-	}
-
-	public void setFoottype(Short foottype) {
-		this.foottype = foottype;
-	}
-
-	public Short getBicycletype() {
-		return bicycletype;
-	}
-
-	public void setBicycletype(Short bicycletype) {
-		this.bicycletype = bicycletype;
-	}
-
-	public Short getSegregatedtype() {
-		return segregatedtype;
-	}
-
-	public void setSegregatedtype(Short segregatedtype) {
-		this.segregatedtype = segregatedtype;
-	}
-
-	public LineString getGeometry() {
-		return geometry;
-	}
-	
-	public void setGeometry(LineString geometry) {
-		this.geometry = geometry;
-	}
 }
