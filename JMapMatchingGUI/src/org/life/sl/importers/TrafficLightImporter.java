f6dcbadd924bfdb1a3f22a73d9bc0ae3e96d1b09
bsnizek <bs@metascapes.org>
2011-10-06 18:35:11 +0200

    + © added

--- JMapMatchingGUI/src/org/life/sl/importers/TrafficLightImporter.java
+++ JMapMatchingGUI/src/org/life/sl/importers/TrafficLightImporter.java
@@ -1,13 +1,28 @@
 package org.life.sl.importers;
 
+/*
+JMapMatcher
+
+Copyright (c) 2011 Bernhard Barkow, Hans Skov-Petersen, Bernhard Snizek and Contributors
+
+mail: bikeability@life.ku.dk
+web: http://www.bikeability.dk
+
+This program is free software; you can redistribute it and/or modify it under 
+the terms of the GNU General Public License as published by the Free Software 
+Foundation; either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT 
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+this program; if not, see <http://www.gnu.org/licenses/>.
+*/
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
-
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.hibernate.Session;
@@ -29,9 +44,6 @@ import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import com.vividsolutions.jts.geom.Coordinate;
--- JMapMatchingGUI/src/org/life/sl/routefinder/DummyShapeFileWriter.java
+++ JMapMatchingGUI/src/org/life/sl/routefinder/DummyShapeFileWriter.java
@@ -1,34 +1,39 @@
 package org.life.sl.routefinder;
 
-//import java.io.BufferedReader;
-//import java.io.File;
-//import java.io.FileReader;
-//import java.io.Serializable;
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.Map;
+/*
+JMapMatcher
+
+Copyright (c) 2011 Bernhard Barkow, Hans Skov-Petersen, Bernhard Snizek and Contributors
+
+mail: bikeability@life.ku.dk
+web: http://www.bikeability.dk
+
+This program is free software; you can redistribute it and/or modify it under 
+the terms of the GNU General Public License as published by the Free Software 
+Foundation; either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT 
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with 
+this program; if not, see <http://www.gnu.org/licenses/>.
+*/
 
 import org.geotools.data.DataUtilities;
-//import org.geotools.data.DefaultTransaction;
-//import org.geotools.data.Transaction;
-//import org.geotools.data.shapefile.ShapefileDataStore;
-//import org.geotools.data.shapefile.ShapefileDataStoreFactory;
+
 import org.geotools.data.simple.SimpleFeatureCollection;
-//import org.geotools.data.simple.SimpleFeatureSource;
-//import org.geotools.data.simple.SimpleFeatureStore;
+
 import org.geotools.feature.FeatureCollections;
 import org.geotools.feature.SchemaException;
 import org.geotools.feature.simple.SimpleFeatureBuilder;
-//import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
-//import org.geotools.geometry.jts.JTSFactoryFinder;
-//import org.geotools.referencing.crs.DefaultGeographicCRS;
+
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
-//import com.vividsolutions.jts.geom.Point;
 
 public class DummyShapeFileWriter {
 
